03457c11e460b3328b98d8c4afb2ce0448a4b6d2
jordan0day <jordan.day@perceptivesoftware.com>
2015-05-01 10:49:02 -0500

    handle missing authorities on web view requests

--- web/controllers/web/authority_controller.ex
+++ web/controllers/web/authority_controller.ex
@@ -1,7 +1,10 @@
 defmodule RouterManager.AuthorityController do
   use RouterManager.Web, :controller
 
+  import Ecto.Query
+
   alias RouterManager.Authority
+  alias RouterManager.Route
 
   plug :scrub_params, "authority" when action in [:create, :update]
   plug :action
@@ -31,8 +34,12 @@ defmodule RouterManager.AuthorityController do
   end
 
   def show(conn, %{"id" => id}) do
-    authority = Repo.get(Authority, id)
-    render conn, "show.html", authority: authority
+    case Repo.get(Authority, id) do
+      nil ->
+        render conn, "not_found.html"
+      authority ->
+        render conn, "show.html", authority: authority
+    end
   end
 
   def edit(conn, %{"id" => id}) do
@@ -42,26 +49,40 @@ defmodule RouterManager.AuthorityController do
   end
 
   def update(conn, %{"id" => id, "authority" => authority_params}) do
-    authority = Repo.get(Authority, id)
-    changeset = Authority.changeset(authority, authority_params)
+    case Repo.get(Authority, id) do
+      nil ->
+        render conn, "not_found.html"
+      authority ->
+        changeset = Authority.changeset(authority, authority_params)
 
-    if changeset.valid? do
-      Repo.update(changeset)
+        if changeset.valid? do
+          Repo.update(changeset)
 
-      conn
-      |> put_flash(:info, "Authority updated successfully.")
-      |> redirect(to: web_authority_path(conn, :index))
-    else
-      render conn, "edit.html", authority: authority, changeset: changeset
+          conn
+          |> put_flash(:info, "Authority updated successfully.")
+          |> redirect(to: web_authority_path(conn, :index))
+        else
+          render conn, "edit.html", authority: authority, changeset: changeset
+        end
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    authority = Repo.get(Authority, id)
-    Repo.delete(authority)
+    case Repo.get(Authority, id) do
+      nil ->
+        render conn, "not_found.html"
+      authority ->
+        Repo.transaction(fn ->
+          Route
+          |> where([r], r.authority_id == ^id)
+          |> Repo.delete_all
 
-    conn
-    |> put_flash(:info, "Authority deleted successfully.")
-    |> redirect(to: web_authority_path(conn, :index))
+          Repo.delete(authority)
+        end)
+
+        conn
+        |> put_flash(:info, "Authority deleted successfully.")
+        |> redirect(to: web_authority_path(conn, :index))
+    end
   end
 end
